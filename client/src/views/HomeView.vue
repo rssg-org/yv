c98cf75a9b9abfacf16c5a61a1bd605926c448f6
resisigu <148083782+resisigu@users.noreply.github.com>
2026-09-06 21:50:52 +0900

    Update HomeView.vue

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -31,7 +31,7 @@
     <footer class="footer">
       <p style="margin-block-start: 1px; color: var(--text-primary);">Cordtube Created by<a href="https://cordwise.org">Cordwise(@vister000)</a></p>
       <p style="margin-block-start: 1px; color: var(--text-primary);">Based on しあtube</p>
-      <div style="color: var(--text-secondary);">v2.1.1</div>
+      <div style="color: var(--text-secondary);">v2.1.2</div>
     </footer>
   </div>
 </template>
